4316999d9a35dccd06a0fd097a6e465fe271e207
Matt Jenkins <matt@majenko.co.uk>
2024-05-30 15:54:54 +0100

    Added documentation comments

--- BitField.gd
+++ BitField.gd
@@ -29,36 +29,56 @@
 extends RefCounted
 class_name BitField
 
+## A binary bitwise flags class
+##
+## This class gives a simple interface to bitwise flags. Simple set and clear
+## operations as well as a bit testing function make this simple to use.
+##
+## A bit is indicated by its offset within the integer - that is bits are
+## numbered from 0 to 63 inclusive.
+
+
+## Emitted whenever a bit, or the whole value, changes.
 signal changed(val : int)
 
+## The raw internal integer representation of the bits.
 var value : int = 0 :
 	set(x):
 		value = x
 		changed.emit(x)
 
+
+## Set a bit. If the optional second parameter is set to false the bit will
+## be cleared instead.
 func set_bit(b : int, v : bool = true) -> void:
 	if v:
 		value |= (1 << b)
 	else:
 		value &= ~(1 << b)
 
+## Clear a bit. If the optional setcond parameter is set to false the bit
+## will be set instead.
 func clear_bit(b : int, v : bool = true) -> void:
 	if v:
 		value &= ~(1 << b)
 	else:
 		value |= (1 << b)
 
+## Toggle a bit. If it's set, clear it. If it's clear, set it.
 func toggle_bit(b : int) -> void:
 	if is_set(b):
 		clear_bit(b)
 	else:
 		set_bit(b)
 
+## Test if a bit is set or not. Returns true if it is, otherwise false.
 func is_set(b) -> bool:
 	return (value & (1 << b)) == (1 << b)
 
+## Set the whole bitfield value
 func set_value(v : int) -> void:
 	value = v
 
+## Get the whole bitfield value
 func get_value() -> int:
 	return value
